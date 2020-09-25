dd78668d466e055c3822240731b1c2d229c9c18d
Chloe Steffens <caforonda103@yahoo.com>
2020-09-25 05:16:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(arraynames, eventname) {
+  let thankYouCards = []
+  for (let i = 0; i < arraynames.length; i++) {
+    thankYouCards.push(`Thank you, ${arraynames[i]}, for the wonderful ${eventname} gift!`);
+  }
+ 
+  return thankYouCards;
+}
+
+function countDown(number){
+  let i = 0;
+  while (i < 11){
+    console.log(i++)
+  }
+}
