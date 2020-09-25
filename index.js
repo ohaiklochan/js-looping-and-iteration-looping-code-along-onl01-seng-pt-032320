8e2d2c3a4215f032aef357e4f3c6eae858199811
Chloe Steffens <caforonda103@yahoo.com>
2020-09-25 05:13:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function writeCards(["Lisa", "Kaitlin", "Jan"], "surprise") {
+function writeCards(arraynames, eventname) {
   for (let i = 0; i < cards.length; i+) {
     console.log(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
   }
