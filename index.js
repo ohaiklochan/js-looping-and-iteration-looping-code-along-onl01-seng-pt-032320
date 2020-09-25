7560d06c649549afd570fe654e5ce9bfd4bc3844
Chloe Steffens <caforonda103@yahoo.com>
2020-09-25 05:06:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const cards = ["teddy bear", "drone", "doll"];
  
-function wrapCards(["Ada", "Brendan", "Ali"], "birthday") {
+function wrapCards(['Lisa', 'Kaitlin', 'Jan'], "birthday") {
   for (let i = 0; i < cards.length; i++) {
     console.log(`Thank you, ${cards[i]}, for the wonderful birthday gift!`);
   }
