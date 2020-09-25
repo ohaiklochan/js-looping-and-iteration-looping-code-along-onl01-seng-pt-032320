d5b160d89127899aafd2a4b9c0e4a769f9e7c7a9
Chloe Steffens <caforonda103@yahoo.com>
2020-09-25 05:13:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function writeCards(arraynames, eventname) {
-  for (let i = 0; i < cards.length; i+) {
+  let thankYouCards = []
+  for (let i = 0; i < cards.length; i++) {
     console.log(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
   }
  
