023c96bd0659141b02914f753c80d0fc3104967c
Chloe Steffens <caforonda103@yahoo.com>
2020-09-25 05:09:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const cards = ["Lisa", "Kaitlin", "Jan"];
  
 function wrapCards(["Lisa", "Kaitlin", "Jan"], "surprise") {
-  for (let i = 0; i < cards.length; i++) {
+  for (let i = 0; i < cards.length; i+) {
     console.log(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
   }
  
